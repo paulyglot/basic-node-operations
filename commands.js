e285b23f017fb91dac8b27b144121d0dddf84f1d
Paul Brower <pabrower@icloud.com>
2018-09-02 22:45:21 -0700

    added head logic and errorHandler still in the works

--- commands.js
+++ commands.js
@@ -25,14 +25,29 @@ const fs = require("fs");
      case "tail":
        commandLibrary.tail(userInputArray.slice(1));
        break;
+
+     case "head":
+       commandLibrary.head(userInputArray.slice(1));
+       break;       
+
+     case "errorHandler":
+       commandLibrary.errorHandler(userInputArray.slice(1));
+       break;
+
     }
  }
 
 //where we will store the logic of our commands
  const commandLibrary = {
 
- 	"echo": function(userInput) {
+ 	 "echo": function(userInput) {
        done(userInput);
+   },
+
+   "errorHandler": function(userInput) {
+       if (userInput !== 'echo' | 'cat' | 'tail' | 'head') {
+          console.log("error: the command cannot be found")
+       }
    },
 
    "cat": function(fullPath) {
@@ -47,9 +62,19 @@ const fs = require("fs");
        const readLastLines = require('read-last-lines');
        readLastLines.read('./commands.js', 10)
         .then((lines) => console.log(lines));
-   }
+   },
 
+    "head": function(fullPath) {
+        const readFirstLines = fullPath[0];
+        console.log(
+          fs.readFileSync(readFirstLines, 'utf8').slice(0, 200)
+      );
+    }
  };
 
  module.exports.commandLibrary = commandLibrary;
  module.exports.evaluateCmd = evaluateCmd;
+
+
+
+
